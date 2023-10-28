3bd346751f601cb9f98b8b4d7d80b2122c82c736
George Waller <georgewaller@gswdh.co.uk>
2023-10-28 22:57:18 +0100

    button task added but will not work as the USB interrupt and attach lines will cause it to time out when the button is not being pressed

--- program/program.c
+++ program/program.c
@@ -8,9 +8,12 @@
 #include "power.h"
 #include "logging.h"
 
+#include "gpio.h"
+
 #define LOG_TAG "PROG"
 
-void blink_task(void * params);
+void blink_task(void *params);
+void button_task(void *params);
 
 void program_entry()
 {
@@ -26,39 +29,82 @@ void program_entry()
 
 	// Get the wake up source
 	pwr_wake_source_t wake_source = pwr_get_wake_source();
-	switch(wake_source)
+	switch (wake_source)
 	{
 	case PWR_WAKE_BUTTON:
+		log_info(LOG_TAG, "Wake source is the button.\n");
 		break;
 
 	case PWR_WAKE_USB:
+		log_info(LOG_TAG, "Wake source is the USB.\n");
 		break;
 
 	case PWR_WAKE_UNKNOWN:
+		log_info(LOG_TAG, "Wake source is unknown.\n");
 		break;
 	}
 
 	// Activity status
 	xTaskCreate(blink_task, "Blink", 1024, NULL, tskIDLE_PRIORITY, NULL);
+	//xTaskCreate(button_task, "Button", 1024, NULL, tskIDLE_PRIORITY, NULL);
 }
 
 void sys_delay(uint32_t t_delay_ms)
 {
-    vTaskDelay(t_delay_ms / portTICK_PERIOD_MS);
+	vTaskDelay(t_delay_ms / portTICK_PERIOD_MS);
 }
 
 uint32_t sys_get_tick()
 {
-    return xTaskGetTickCount();
+	return xTaskGetTickCount();
 }
 
-void blink_task(void * params)
+void blink_task(void *params)
 {
-	while(1)
+	while (1)
 	{
 		HAL_GPIO_WritePin(PMC_LED_RUN_GPIO_Port, PMC_LED_RUN_Pin, true);
 		sys_delay(1);
 		HAL_GPIO_WritePin(PMC_LED_RUN_GPIO_Port, PMC_LED_RUN_Pin, false);
 		sys_delay(99);
+	}
+}
+
+void button_task(void *params)
+{
+	uint32_t tick = sys_get_tick();
+
+	while (1)
+	{
+		if (sys_get_tick() > (tick + 1000))
+		{
+			tick = sys_get_tick();
+
+			// Stay in the loop while the button is held
+			while (HAL_GPIO_ReadPin(SYS_WAKE_GPIO_Port, SYS_WAKE_Pin))
+			{
+				// If it's held longer than 2s
+				if (sys_get_tick() > tick + 2000)
+				{
+					// Set the LED indicator
+					HAL_GPIO_WritePin(PMC_LED_STATUS_GPIO_Port, PMC_LED_STATUS_Pin, 1);
+
+					// Wait for the button to be released
+					while (HAL_GPIO_ReadPin(SYS_WAKE_GPIO_Port, SYS_WAKE_Pin))
+					{
+						sys_delay(10);
+					}
+
+					// Turn the system off
+					pwr_sys_off();
+
+					// Wait for that to happen
+					sys_delay(100);
+
+					// Sleep the system
+					pwr_sleep();
+				}
+			}
+		}
 	}
 }
